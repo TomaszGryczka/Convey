16e1dc6af1ecaecb45f114b5461912f9bac47d37
Tomasz Gryczka <tumek001@gmail.com>
2022-02-18 17:56:16 +0100

    Add initial files

---
+++ src/main/java/com/github/tomaszgryczka/convey/ConveyApplication.java
@@ -0,0 +1,13 @@
+package com.github.tomaszgryczka.convey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tomaszgryczka/convey/ConveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tomaszgryczka.convey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
